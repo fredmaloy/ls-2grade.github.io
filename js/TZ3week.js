7002ad0bb146c2db58a04c372126e92b8556d03a
Fred Maloy <fredmaloy@gmail.com>
2019-02-07 10:41:50 +0500

    section_team edited padding photo

---
+++ js/TZ3week.js
@@ -0,0 +1,73 @@
+/* var name = 'Сергей';
+console.log(name);
+
+name = 'Рафа';
+console.log(name); */
+
+/* if (4>5) {
+console.log ('Верно');
+} else {
+    console.log ('Ложь');
+} */
+
+/* for (i=0; i<10; i++) {
+console.log ( i );
+} */
+
+/* function sum(p1, p2, p3) {
+    return p1 + p2 + p3;
+}
+
+var a=sum(1, 2, 3);
+var b=sum(10, 20, 30);
+var c=sum(20, 40, 60);
+
+console.log(a)
+console.log(b)
+console.log(c) */
+
+/* var d = ['Привет','loftschool'];
+d.push('Я изучаю','javascript');
+console.log(d[2]);
+console.log(d.length);
+for (var i=0; i<d.length; i++) {
+    console.log(i);
+} */
+
+/* var array=[10,120,150,35,45,672,85,63,722,27];
+for (var i=0;i<array.length;i++) {
+    if (array[i]>100) {
+        console.log(array[i]);
+    }
+} */
+
+var form={
+    name: 'Сергей',
+    lastName: 'Черепанов',
+    age: '30'
+}
+
+console.log(form.name);
+console.log(form.lastName);
+console.log(form.age);
+
+form.mail='fredmaloy@mail.ru';
+
+console.log(form.mail);
+
+var obj={
+    name: 'Сергей',
+    lastName: 'Черепанов',
+    age: '30'
+    }
+
+function hello(human) {
+    var human = 'Привет, ' + 'меня ' + 'зовут ' + human.name + ' ' + human.lastName + ' '+ 'и ' + 'мне ' + human.age + ' ' + 'лет!';
+    return human;
+}
+
+var result = hello(form) 
+
+    console.log(result)
+
+    
