856a8a27e7f91d1f1d0590e66388cc5643be0fef
Sergey <fredmaloy@gmail.com>
2019-09-25 21:30:07 +0500

    борьба с метками на карте

--- js/map.js
+++ js/map.js
@@ -20,7 +20,7 @@
                 // Необходимо указать данный тип макета.
                 iconLayout: 'default#image',
                 // Своё изображение иконки метки.
-                iconImageHref: '../icons/map-marker.svg',
+                iconImageHref: './icons/map-marker.svg',
                 // Размеры метки.
                 iconImageSize: [48, 48],
                 // Смещение левого верхнего угла иконки относительно
@@ -37,7 +37,7 @@
                 // Необходимо указать данный тип макета.
                 iconLayout: 'default#imageWithContent',
                 // Своё изображение иконки метки.
-                iconImageHref: '../icons/map-marker.svg',
+                iconImageHref: '../../icons/map-marker.svg',
                 // Размеры метки.
                 iconImageSize: [48, 48],
                 // Смещение левого верхнего угла иконки относительно
